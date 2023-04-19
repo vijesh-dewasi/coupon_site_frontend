f966749c1723db191d774eb508c97f25eb79602f
vijesh-dewasi <gvijeshkumar@gmail.com>
2023-04-20 01:28:10 +0530

    added the coupon_Tag_images and aligned coupon

--- src/components/coupon.jsx
+++ src/components/coupon.jsx
@@ -19,75 +19,95 @@ function Coupon(props) {
     availability,
     redirection_link } = props.coupon;
 
-    const [expand_visible,set_expand]=useState(0);
+  const [expand_visible, set_expand] = useState(0);
 
-    function Expanded_coupon(){
+  function Expanded_coupon() {
 
-      function copy_button_handler(e){
-        navigator.clipboard.writeText(coupon_code)
-       e.target.innerText="copied";
-      }
+    function copy_button_handler(e) {
+      navigator.clipboard.writeText(coupon_code)
+      e.target.innerText = "copied";
+    }
 
-      return (
-        <div className="exp_overlay" style={{display:expand_visible?"inline":"none"}}>
-       
-       <div className="expanded_coupon flex_col">
-      
-      <div  className="cross_mark">
-      <a onClick={()=>(set_expand(0))}>
-      <i className="fa-regular fa-circle-xmark"></i>
-      </a>
-      </div>
-                    
+    return (
+      <div className="exp_overlay" style={{ display: expand_visible ? "inline" : "none" }}>
+
+        <div className="expanded_coupon flex_col">
+
+          <div className="cross_mark">
+            <a onClick={() => (set_expand(0))}>
+              <i className="fa-regular fa-circle-xmark"></i>
+            </a>
+          </div>
+
           <div className="detail_logo ">
             <div className="logo_img_holder">
               <img className="logo_img" src={logo_url} alt="kuch_bhi" />
             </div>
-            
+
             <div className='coupon_desc flex_col'>
               <h3>
                 {coupon_title}
               </h3>
               <p className="">{coupon_desc}</p>
             </div>
-    
+
           </div>
-    
+
           <div className="copy_code flex_row">
-    
-              <div className="flex_row coupon_copier">
+
+            <div className="flex_row coupon_copier">
               <div className="coupon_code">
                 {coupon_code}
               </div>
-              <button  onClick={copy_button_handler} className="copy_btn">Copy Code</button>
-              </div>
+              <button onClick={copy_button_handler} className="copy_btn">Copy Code</button>
+            </div>
           </div>
           <a href={redirection_link}>Visit Site</a>
         </div>
 
 
-        </div> 
-      )
-    }
+      </div>
+    )
+  }
 
 
   return (
-    
+
     <div className='container flex_col'>
       <Expanded_coupon />
       <div className='coupon_info_bar flex_row'>
 
-        <p className="coupon_tag">
-          {isverified ? "verfied":""}
-        </p>
+        { isverified?
+        <div className="coupon_tag flex_row">
+           <p>Verfied</p>
+          <div className="tag_img_holder">
+           <img className="tag_img" src="./images/verified.png" alt="" />
+          </div>
+        </div>:""
+        }
 
-        <p className="coupon_tag">
-          {islimited ? "exclusive" : "regular"}
-        </p>
+        {islimited ?
+        <div className="coupon_tag flex_row">
+          <p >
+             Exclusive 
+          </p>
+          <div className="tag_img_holder">
+          <img className="tag_img" src="./images/limited.png" alt="" />
+          </div>
+        </div>: "regular"
+        }
+        {(brand.length > 0) ?
+        <div className="coupon_tag flex_row">
+          <p>
+             {brand} 
+          </p>
+          <div className="tag_img_holder">
+           <img className="tag_img" src="./images/brand-image.png" alt="" />
+          </div>
+        </div>
+        : ""}
 
-        <p className="coupon_tag">
-          {(brand.length > 0) ? brand : ""}
-        </p>
+
 
       </div>
 
@@ -106,7 +126,7 @@ function Coupon(props) {
             <div className="logo_img_holder">
               <img className="logo_img" src={logo_url} alt="kuch_bhi" />
             </div>
-            <button onClick={()=>(set_expand(1))} className='fancy_button' href="#">check coupon</button>
+            <button onClick={() => (set_expand(1))} className='fancy_button' href="#">check coupon</button>
           </div>
 
         </div>
